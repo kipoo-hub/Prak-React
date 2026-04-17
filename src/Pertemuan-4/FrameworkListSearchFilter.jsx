af991ba2705802f069c9f8948c4b5c2801b2593b
kipoo-hub <taufiq24si@mahasiswa.pcr>
2026-04-17 16:09:14 +0700

    Pertemuan 4 : Responsive & Grid design

--- src/Pertemuan-4/FrameworkListSearchFilter.jsx
+++ src/Pertemuan-4/FrameworkListSearchFilter.jsx
@@ -3,11 +3,27 @@ import frameworkData from "./framework.json";
 
 export default function FrameworkListSearchFilter() {
   /** Deklrasai state **/
-  const [searchTerm, setSearchTerm] = useState("");
-  const [selectedTag, setSelectedTag] = useState("");
+  // const [searchTerm, setSearchTerm] = useState("");
+  // const [selectedTag, setSelectedTag] = useState("");
+
+  /*Inisialisasi DataForm*/
+		const [dataForm, setDataForm] = useState({
+			searchTerm: "",
+			selectedTag: "",
+			/*Tambah state lain beserta default value*/
+			});
+		
+		/*Inisialisasi Handle perubahan nilai input form*/
+		const handleChange = (evt) => {
+			const { name, value } = evt.target;
+			setDataForm({
+				...dataForm,
+				[name]: value,
+			});
+		};
   
   /** Deklrasai Logic Search & Filter **/
-  const _searchTerm = searchTerm.toLowerCase();
+  const _searchTerm = dataForm.searchTerm.toLowerCase();
   const filteredFrameworks = frameworkData.filter((framework) => {
     const matchesSearch =
       framework.name
@@ -17,7 +33,7 @@ export default function FrameworkListSearchFilter() {
 				.toLowerCase()
 				.includes(_searchTerm);
 
-    const matchesTag = selectedTag ? framework.tags.includes(selectedTag) : true;
+    const matchesTag = dataForm.selectedTag ? framework.tags.includes(selectedTag) : true;
 
     return matchesSearch && matchesTag;
   });
@@ -43,12 +59,13 @@ export default function FrameworkListSearchFilter() {
         name="searchTerm"
         placeholder="Search framework..."
         className="w-full p-2 border border-gray-300 rounded mb-4"
-        onChange={(e) => setSearchTerm(e.target.value)}
+        onChange={handleChange}
       />
 
       <select
         name="selectedTag"
         className="w-full p-2 border border-gray-300 rounded mb-4"
+        onChange={handleChange}
       >
        <option value="">All Tags</option>
         {allTags.map((tag, index) => (
--- src/Pertemuan-4/main.jsx
+++ src/Pertemuan-4/main.jsx
@@ -3,10 +3,13 @@ import './tailwind.css'
 import FrameworkList from "./FrameworkList";
 import FrameworkListSearchFilter from "./FrameworkListSearchFilter";
 
+
 createRoot(document.getElementById("root"))
     .render(
         <div>
            {/* <FrameworkList/>  */}
-           <FrameworkListSearchFilter/>
+           {/* <FrameworkListSearchFilter/> */}
+           
+           <ResponsiveDesign/>
         </div>
     )
